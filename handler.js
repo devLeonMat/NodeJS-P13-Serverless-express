449130ae1d6140c50b25bbf302914a42cbc7d78d
leon matias <roslin.pineda@nautilus.team>
2021-06-07 01:24:09 -0500

    cambios

--- handler.js
+++ handler.js
@@ -3,9 +3,21 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const AWS = require('aws-sdk');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('Hola mundo con express js');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+app.use(bodyParser.json({string: false}));
+
+
+app.get('/',(req, res) => {
+    res.send('Hola mundo con express');
+});
+
+app.post('/users',(req, res) => {
+    const {userId, name}= req.body;
+    res.json({userId, name})
 });
 
 module.exports.generic = serverless(app);
